a21c61afd6ad7b24de815160f8fbb8ea3bdc0c58
gengbin <1371103410@qq.com>
2025-12-22 17:18:41 +0800

    部分组件调整

--- packages/e-custom-table/e-custom-table.vue
+++ packages/e-custom-table/e-custom-table.vue
@@ -1,20 +1,9 @@
 <template>
   <el-table :data="listComputed">
+    <el-table-column label="123" prop="first"></el-table-column>
     <template v-for="(item, index) in listColumn">
-      <el-table-column
-        v-if="!item.custom"
-        :key="index + ''"
-        :fixed="item.fixed"
-        :width="item.width"
-        v-bind="item"
-      ></el-table-column>
-      <el-table-column
-        v-else
-        :key="index + '2'"
-        :fixed="item.fixed"
-        :width="item.width"
-        v-bind="item"
-      >
+      <el-table-column v-if="!item.custom" :key="index + ''" v-bind="item"></el-table-column>
+      <el-table-column v-else :key="index + '2'" v-bind="item">
         <template #default="{ $index }">
           <slot :name="item.key" :index="$index"></slot>
         </template>
@@ -23,33 +12,28 @@
   </el-table>
 </template>
 <script>
-import Vue from 'vue'
-import { Table, TableColumn } from 'element-ui'
-Object.entries({ Table, TableColumn }).forEach(item=>{
-  Vue.use(item[1])
-})
-  export default {
-    name: 'E_CustomTable',
-    props: {
-      list: {
-        type: Array,
-        default: () => [],
-      },
-      listColumn: {
-        type: Array,
-        default: () => [],
-        /**
-         * custom
-         * key
-         * name
-         */
-      },
+export default {
+  name: 'E_CustomTable',
+  props: {
+    list: {
+      type: Array,
+      default: () => [],
     },
-    computed: {
-      listComputed() {
-        return this.list
-      },
+    listColumn: {
+      type: Array,
+      default: () => [],
+      /**
+       * custom
+       * key
+       * name
+       */
     },
-  }
+  },
+  computed: {
+    listComputed() {
+      return this.list
+    },
+  },
+}
 </script>
 <style lang="scss" scoped></style>
--- packages/e-custom-table/index.js
+++ packages/e-custom-table/index.js
@@ -1,7 +1,11 @@
 import E_CustomTable from "./e-custom-table.vue"
-E_CustomTable.install = function(Vue){
+import { Table, TableColumn } from 'element-ui';
+E_CustomTable.install = function (Vue) {
+  [Table, TableColumn].forEach((item) => {
+    Vue.use(item)
+  })
   Vue.component(E_CustomTable.name, E_CustomTable)
 }
 export {
-  E_CustomTable
+  E_CustomTable,
 }
