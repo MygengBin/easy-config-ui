3849b411b1f78f51ba224f28ca169ab398de167e
gengbin <1371103410@qq.com>
2026-04-01 17:52:19 +0800

    临时变更

--- packages/e-custom-form/index.js
+++ packages/e-custom-form/index.js
@@ -1,9 +1,17 @@
 import E_CustomForm from "./e-custom-form.vue"
+/* 
+2的版本
 import { Form, FormItem, Input, Select, Option } from 'element-ui';
 E_CustomForm.install = function(Vue){
   [Form, FormItem, Input, Select, Option].forEach((item) => Vue.use(item))
   Vue.component(E_CustomForm.name, E_CustomForm)
-}
+} 
+  */
+import { ElForm, ElFormItem, ElInput, ElSelect, ElOption } from 'element-plus';
+E_CustomForm.install = function(Vue){
+  [ElForm, ElFormItem, ElInput, ElSelect, ElOption].forEach((item) => Vue.use(item))
+  Vue.component(E_CustomForm.name, E_CustomForm)
+} 
 export {
   E_CustomForm,
 }
